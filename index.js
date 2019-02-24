0da547d5b18f018b094446e29e7db88f1445aa88
Rodrigo Santos <rodrigosantos@bleu.com.br>
2019-02-24 02:20:21 -0300

    index js node express

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/twitch'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+res.sendFile(path.join(__dirname+'/dist/twitch/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
